fad730461f438afa58421b93fc1b95eca22f2d23
Daniel Thorpe <dan@blindingskies.com>
2013-03-10 17:24:02 +0000

    Initial Commit

---
+++ DNTSlidingPanes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DNTSlidingPanes
+//
+//  Created by Daniel Thorpe on 10/03/2013.
+//  Copyright (c) 2013 Daniel Thorpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DNTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DNTAppDelegate class]));
+    }
+}
---
+++ DNTSlidingPanesTests/DNTSlidingPanesTests.m
@@ -0,0 +1,32 @@
+//
+//  DNTSlidingPanesTests.m
+//  DNTSlidingPanesTests
+//
+//  Created by Daniel Thorpe on 10/03/2013.
+//  Copyright (c) 2013 Daniel Thorpe. All rights reserved.
+//
+
+#import "DNTSlidingPanesTests.h"
+
+@implementation DNTSlidingPanesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DNTSlidingPanesTests");
+}
+
+@end
